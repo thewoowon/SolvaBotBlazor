8723ac496f9b5c93785f1e1c4613c1124ad0d929
thewoowon <thewoowon@naver.com>
2022-04-29 09:30:11 +0900

    초기 커밋.

---
+++ Data/ParameterFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolvaBot.Data
+{
+    public class ParameterFinder
+    {
+        public Task GetForecastAsync()
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+
+    }
+}
